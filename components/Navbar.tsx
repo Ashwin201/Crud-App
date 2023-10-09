18309e086e3b5f97883e9b8ae85322d91d7c5d91
Ashwin Sharma <90189100+Ashwin201@users.noreply.github.com>
2023-10-10 01:28:12 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,13 +14,7 @@ const Navbar = () => {
                 >
                     <LuListTodo size={35} />
                 </Link>
-                <div className=" flex items-center">
-                    <Link
-                        href={"/"}
-                        className="  text-white font-medium   hover:text-green-600 transition-all duration-300"
-                    >
-                        Home
-                    </Link>
+                   
                     <Link
                         href={"/addpost"}
                         className="  px-3 ml-3 py-[4px] md:py-[6px] bg-black text-white font-medium rounded-lg border-2 border-white  hover:bg-white  hover:text-black transition-all duration-300"
@@ -29,7 +23,6 @@ const Navbar = () => {
                     </Link>
 
 
-                </div>
 
 
 
